bfe96547dec06ffd5b74da1c3c4b293766bfdbb9
Yaroslav <yarspirin@yandex-team.ru>
2019-01-25 13:23:37 +0300

    add package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "JASON",
+    products: [
+        .library(
+            name: "JASON",
+            targets: ["JASON"]),
+        ],
+    targets: [
+        .target(
+            name: "JASON",
+            path: "Source"),
+        .testTarget(
+            name: "JASOMTests",
+            dependencies: ["PMJSON"]),
+        ]
+)
